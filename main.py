34e0e4a2666a53f7077df797a9c2be3ec15f37de
khomesh24 <khomeshthakre24@gmail.com>
2017-10-11 19:46:03 +0530

    add main.py this python script just take input form user and create VM create command

---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+class create:
+    def __init__(self):
+        self.name=""
+        self.ram=1024
+        self.cpus=1
+        self.disk_path="/var/lib/libvirt/images/"
+        self.size=10
+        self.bus="virtio"
+        self.location="http:192.168.0.203/repos/fedora25/"
+        self.net_adp="virbr0"
+
+    def get_name(self):
+        self.name=input("Enter name of VM: ")
+
+    def get_ram(self):
+        self.ram=int(input("Enter RAM size(MB): "))
+
+    def get_cpus(self):
+        self.cpus=int(input("Enter number of vcpus: "))
+
+    def get_size(self):
+        self.size=int(input("Enter size you want to allocate to VM(GiB): "))
+
+    def final(self):
+        print("virt-install "\
+              " --name "+self.name+ \
+              " --ram "+str(self.ram)+ \
+              " --vcpus "+str(self.cpus)+ \
+              " --disk path="+self.disk_path+self.name+".qcow2,bus="+self.bus+",size="+str(self.size)+ \
+              " --location "+self .location+ \
+              " --network bridge:"+str(self.net_adp))
+
+if __name__ == '__main__':
+    a = create()
+    a.get_name()
+    a.get_ram()
+    a.get_cpus()
+    a.get_size()
+    a.final()
